ed83ca96af3c5c2a2eecf52eeedf4f29607d68ce
msanteramo <33138631+msanteramo@users.noreply.github.com>
2017-10-26 20:34:21 -0400

    Second push

--- projectA/src/main/java/Main.java
+++ projectA/src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String message = "Hello Lisa!";
+        String message = "Hello Lisa Lisa Lisa!";
         System.out.println(message);
     }
 }
